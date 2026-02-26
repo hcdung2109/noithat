b0dbf087e4ed13b609e2bb11a2c1d53c16d9f6df
DESKTOP-G94FLTS\hcdung <hcdung@vnpt.vn>
2026-02-26 17:50:00 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ConsultationRequestController;
 use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Admin\ConsultationRequestController as AdminConsultationRequestController;
 use App\Http\Controllers\Admin\AuthController as AdminAuthController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\SiteSettingController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/gioi-thieu', [HomeController::class, 'about'])->name('about');
 
 Route::get('/du-an', [ProjectController::class, 'index'])->name('projects.index');
 Route::get('/du-an/{project}', [ProjectController::class, 'show'])->name('projects.show');
+Route::post('/consultations', [ConsultationRequestController::class, 'store'])->name('consultations.store');
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::middleware('guest')->group(function () {
@@ -22,6 +26,9 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::middleware('auth')->group(function () {
         Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/settings', [SiteSettingController::class, 'edit'])->name('settings.edit');
+        Route::put('/settings', [SiteSettingController::class, 'update'])->name('settings.update');
+        Route::get('/consultations', [AdminConsultationRequestController::class, 'index'])->name('consultations.index');
         Route::resource('projects', AdminProjectController::class);
     });
 });
